d37a889186d47c476f6ac83b9785f4f33869ff54
hanuman <a.kolyan@hanuman.am>
2017-10-06 09:55:22 +0000

    quee changed

--- setup.sh
+++ setup.sh
@@ -1,17 +1,19 @@
+sudo apt-get install git
+sudo bash -c 'echo "Asia/Yerevan" > /etc/timezone'
+sudo dpkg-reconfigure keyboard-configuration
+cd ~/Desktop && git clone https://gitlab.com/a.kolyan/tumo-helper-scripts.git
+
 cd /tmp
 mkdir /tmp/workingwith
-sudo apt-get install git jq keepassx terminator fish curl chromium-browser mplayer
+sudo apt-get install jq keepassx terminator fish curl chromium-browser mplayer
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install npm
 sudo npm install -g yarn
 
-sudo bash -c 'echo "Asia/Yerevan" > /etc/timezone'
-sudo dpkg-reconfigure keyboard-configuration
-
 git config --global user.email "a.kolyan@hanuman.am"
-git config --global user.name "hanumanum"
+git config --global user.name "hanuman"
 
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -20,7 +22,5 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 sudo apt-get update
 sudo apt-get install code # or code-insiders
 
-
-cd ~/Desktop && git clone https://gitlab.com/a.kolyan/tumo-helper-scripts.git
 
 terminator & exit
