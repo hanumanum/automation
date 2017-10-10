3ba6ef0161b186980cf62bd31519a16e0cf35a8c
hanuman <a.kolyan@hanuman.am>
2017-10-10 08:26:41 +0000

    configs added

--- setup.sh
+++ setup.sh
@@ -9,19 +9,18 @@ sudo apt-get install jq keepassx terminator fish curl chromium-browser mplayer
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo apt-get install npm
+sudo chown -R $USER:$(id -gn $USER) /home/kubuntu/.config
+#sudo apt-get install npm #may be not necessary
 sudo npm install -g yarn
 
 git config --global user.email "a.kolyan@hanuman.am"
 git config --global user.name "hanuman"
-
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get install code # or code-insiders
-
-cp configs/okularpartrc /home/kubuntu/.config/
+cp -R  /media/kubuntu/USB\ DISK/automation/configs/Code/User/ /home/kubuntu/.config/Code/
 
 terminator & exit
