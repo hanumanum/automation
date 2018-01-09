8124274a7588e97d67860b1e33cbe62ea95aa5be
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-01-09 12:56:30 +0000

    dolpin places

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+sudo apt-get update
+sudo apt-get -y install firefox
 sudo apt-get -y install git
 bash setup.git.sh
 sudo bash -c 'echo "Asia/Yerevan" > /etc/timezone'
@@ -8,7 +10,6 @@ cd /tmp
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update 
 sudo apt-get -y install google-chrome-stable
 
 
@@ -28,9 +29,11 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 sudo apt-get -y install code # or code-insiders
-sudo apt-get install firefox
 cp -R  /media/kubuntu/USB\ DISK/automation/configs/Code/User/ /home/kubuntu/.config/Code/
 cp -R  /media/kubuntu/USB\ DISK/automation/configs/keepassx/ /home/kubuntu/.config/
+cp configs/user-places.xbel /home/kubuntu/.local/share/
+
 sudo apt-get -y install shutter
+sudo apt-get -y install gimp
 sudo apt autoremove -y
 terminator --execute 'fish' & exit
