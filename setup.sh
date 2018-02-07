b859bdc658a643d7ca4d53803ab9964705f792dc
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-02-07 07:02:52 +0000

    chrome - chromium

--- setup.sh
+++ setup.sh
@@ -36,7 +36,7 @@ sudo apt autoremove -y
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get -y install google-chrome-stable
-
+sudo apt-get -y install chromium-browser
+alias google-chrome="chromium-browser"
 terminator --execute 'fish' & exit
 
