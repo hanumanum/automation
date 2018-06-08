b5079f3c7bfced4ae6ae46446b5454749fd4241a
artak kolyan (aka hanuman) <a.kolyan@gratun.am>
2018-06-08 09:04:08 +0000

    moc added, keepass ini

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
-xrandr --output eDP --mode 1920x1080 --rate 60
+#xrandr --output eDP --mode 1920x1080 --rate 60
 sudo cp /media/kubuntu/USB\ DISK/automation/configs/sources.list /etc/apt/
 sudo add-apt-repository ppa:peek-developers/stable -y
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
 sudo apt-get update
 sudo apt-get -y install firefox
@@ -12,9 +16,10 @@ sudo dpkg-reconfigure keyboard-configuration
 cd ~/Desktop && git clone https://gitlab.com/a.kolyan/tumo-helper-scripts.git
 cd /tmp
 
+
 mkdir /tmp/workingwith
 #sudo apt-get -y chromium-browser
-sudo apt-get -y install jq keepassx terminator fish curl mplayer htop kazam
+sudo apt-get -y install jq keepassx terminator fish curl htop kazam
 
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -24,10 +29,6 @@ sudo install npm
 sudo npm install -g nodemon
 sudo ln -s "$(which nodejs)" /usr/bin/node
 
-curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
 sudo apt-get -y install code # or code-insiders
 cp -R  /media/kubuntu/USB\ DISK/automation/configs/Code/User/ /home/kubuntu/.config/Code/
 cp -R  /media/kubuntu/USB\ DISK/automation/configs/kazam/ /home/kubuntu/.config/kazam/
@@ -37,8 +38,9 @@ cp /media/kubuntu/USB\ DISK/automation/configs/okularpartrc /home/kubuntu/.confi
 cp -R "/media/kubuntu/USB DISK/automation/jsproject_template" ~/Desktop/
 
 sudo apt-get -y install shutter
-sudo apt-get -y install gimp
+#sudo apt-get -y install gimp
 sudo apt-get -y install peek
+sudo apt-get -y install moc
 sudo apt autoremove -y
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -47,7 +49,7 @@ sudo apt-get -y install chromium-browser
 alias google-chrome="chromium-browser"
 alias tumo-helper-scripts="cd /home/kubuntu/Desktop/tumo-helper-scripts/"
 alias usb="cd '/media/kubuntu/USB DISK/'"
-sudo apt remove konsole
+sudo apt remove -y konsole
 
 tar xvfJ  "/media/kubuntu/USB DISK/soft/tsetup.1.2.15.tar.xz" -C /home/kubuntu/
 
